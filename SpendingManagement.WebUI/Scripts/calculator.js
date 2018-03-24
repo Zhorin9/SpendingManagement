3a2f2a93c4662e700b532331cb438a31c066b8ca
Przemyslaw <przemek7799@gmail.com>
2018-03-24 20:31:29 +0100

    Added calculator

--- SpendingManagement.WebUI/Scripts/calculator.js
+++ SpendingManagement.WebUI/Scripts/calculator.js
@@ -1,7 +1,96 @@
 ﻿var currentValue = 0;
 var earlierValue = 0;
 var currentOperator = '';
-function insertMark(sign) {
+
+document.addEventListener('keydown', function (event) {
+    switch (event.keyCode) {                            //ancii code
+        case 13:                                        //numpad enter
+            calculateResult();
+            break;
+        //Numbers 0-1 on keyboard 
+        case 48:
+            insertSign(0);
+            break;
+        case 49:
+            insertSign(1);
+            break;
+        case 50:
+            insertSign(2);
+            break;
+        case 51:
+            insertSign(3);
+            break;
+        case 52:
+            insertSign(4);
+            break;
+        case 53:
+            insertSign(5);
+            break;
+        case 54:
+            insertSign(6);
+            break;
+        case 55:
+            insertSign(7);
+            break;
+        case 56:
+            insertSign(8);
+            break;
+        case 57:
+            insertSign(9);
+            break;
+
+        //NumPad 0-9
+        case 96:
+            insertSign(0);
+            break;
+        case 97:
+            insertSign(1);
+            break;
+        case 98:
+            insertSign(2);
+            break;
+        case 99:
+            insertSign(3);
+            break;
+        case 100:
+            insertSign(4);
+            break;
+        case 101:
+            insertSign(5);
+            break;
+        case 102:
+            insertSign(6);
+            break;
+        case 102:
+            insertSign(7);
+            break;
+        case 103:
+            insertSign(8);
+            break;
+        case 104:
+            insertSign(9);
+            break;
+
+        //Numpad Operators and key ","
+        case 106:
+            changeOperator('*');
+            break;
+        case 107:
+            changeOperator('+');
+            break;
+        case 109:
+            changeOperator('-');
+            break;
+        case 110:
+            insertSign('.');
+            break;
+        case 111:
+            changeOperator('/');
+            break;
+ 
+    }
+})
+function insertSign(sign) {
             var numberTextBox = document.getElementById("displayRowThree");
             if (numberTextBox.value.length < 20) {
                 if (sign != '.') {
@@ -35,7 +124,7 @@ function insertMark(sign) {
                 }
             }
         }
-function changeSign() {
+function changeNumberSign() {
             var display = document.getElementById("displayRowThree");
             if (display.value[0] == '-') {
                 display.value = display.value.slice(1, display.value.length);
@@ -68,7 +157,7 @@ function executeOperation() {
     var earlierNumberTextBox = document.getElementById("displayRowOne");
     currentValue = numberTextBox.value;
     if (earlierValue != '' && numberTextBox.value != '') {
-        earlierValue = calculate();
+        earlierValue = doOperation();
 
         earlierNumberTextBox.value += ' ' + numberTextBox.value + ' ' + '= ';
         resultTextBox.value = earlierValue;
@@ -77,7 +166,7 @@ function executeOperation() {
 
     }
 }
-function calculate() {
+function doOperation() {
     var result = 0;
     switch (currentOperator) {
         case '-':
@@ -109,8 +198,10 @@ function calculateResult() {
     var earlierNumberTextBox = document.getElementById("displayRowOne");
     if (numberTextBox.value != '' && earlierNumberTextBox.value != '' && currentOperator != '') {
         currentValue = numberTextBox.value;
-        resultTextBox.value = calculate();
-        numberTextBox.value = '';
+        numberTextBox.value = doOperation();
+        earlierNumberTextBox.value = '';
+        earlierValue = 0;
+        resultTextBox.value = '';
     } 
 }
 
@@ -118,6 +209,9 @@ function calculateResult() {
 function deleteLastMark() {
     var display = document.getElementById("displayRowThree");
     display.value = display.value.slice(0, - 1);
+    if (display.value[0] == '-' && display.value.length == 1) {
+        display.value = '';
+    }
 }
 function clearAll() {
     document.getElementById("displayRowThree").value = '';
